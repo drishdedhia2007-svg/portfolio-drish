115353dcf63e75e70acdd1012d0c0a468ea97fad
Drish Dedhia <drishdedhia2007@gmail.com>
2026-08-09 19:04:21 +0200

    Add basic hero section with name and title

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <h1 className="text-4xl font-bold text-black">Drish Dedhia</h1>
+      <p className="text-xl text-zinc-600 mt-2">
+        Mechanical Engineering Student @ RWTH Aachen
+      </p>
+    </div>
+  );
+}
